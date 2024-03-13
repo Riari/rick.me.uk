46dc033fec58348051f2c36c7a8f41ab7ef1bb8b
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-03-13 20:23:46 +0000

    Fix post month in URLs

--- src/util/collection.ts
+++ src/util/collection.ts
@@ -6,9 +6,10 @@ export async function loadAndFormatCollection(name) {
     posts.forEach(post => {
         const date = new Date(post.data.pubDate);
         const year = date.getFullYear();
-        const month = (date.getMonth() < 10 ? '0' : '') + date.getMonth();
+        const month = date.getMonth() + 1;
+        const monthZerofilled = (month < 10 ? '0' : '') + month;
 
-        post.slug = `${year}/${month}/${post.slug}`;
+        post.slug = `${year}/${monthZerofilled}/${post.slug}`;
         post.tags = post.data.tags;
     });
 
