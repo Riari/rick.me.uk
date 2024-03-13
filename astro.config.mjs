60a2b341720c5d29d8567ef4add4a65275889612
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-03-13 18:45:33 +0000

    Fix Shiki themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,26 +8,14 @@ export default defineConfig({
     site: 'https://www.rick.me.uk/',
     integrations: [mdx(), sitemap()],
     markdown: {
+        // https://shiki.style/
         shikiConfig: {
-            // Choose from Shiki's built-in themes (or add your own)
-            // https://shiki.style/themes
-            // theme: 'github-light',
-            // Alternatively, provide multiple themes
-            // https://shiki.style/guide/dual-themes
             themes: {
                 light: 'github-light',
                 dark: 'material-theme-palenight',
             },
             defaultColor: 'light',
-            // Add custom languages
-            // Note: Shiki has countless langs built-in, including .astro!
-            // https://shiki.style/languages
-            langs: [],
-            // Enable word wrap to prevent horizontal scrolling
             wrap: false,
-            // Add custom transformers: https://shiki.style/guide/transformers
-            // Find common transformers: https://shiki.style/packages/transformers
-            transformers: [],
         },
     },
 });
