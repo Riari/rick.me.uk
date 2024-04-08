7fcbcf14eb86bb68ac69a4e1140ebd0529cbc3e4
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-04-08 23:25:56 +0100

    Add astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.rick.me.uk/',
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), robotsTxt()],
     markdown: {
         // https://shiki.style/
         shikiConfig: {
