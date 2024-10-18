67adbd951c65e9b7cdc8601657f5f39deb60d5fa
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-10-19 00:13:27 +0100

    Improve navigation and theme selector

---
+++ src/js/theme.ts
@@ -0,0 +1,40 @@
+import mediumZoom, { type Zoom } from "medium-zoom";
+
+const themeRadios: NodeListOf<HTMLInputElement> = document.querySelectorAll("input[type=radio][name=theme]");
+const mediumZoomBackgroundLight = "#fff";
+const mediumZoomBackgroundDark = "#21212b";
+var zoom: Zoom;
+
+function applyTheme(name: string) {
+    document.documentElement.dataset.theme = name;
+
+    const prefersDark = (name == "system" && window.matchMedia("(prefers-color-scheme: dark)").matches)
+        || name == "dark";
+
+    zoom.update({
+        background: prefersDark
+            ? mediumZoomBackgroundDark
+            : mediumZoomBackgroundLight,
+    });
+}
+
+const theme: string = localStorage.getItem("theme") ?? "system";
+
+const themeIndexMap: Map<string, number> = new Map();
+themeIndexMap.set("system", 0);
+themeIndexMap.set("dark", 1);
+themeIndexMap.set("light", 2);
+
+themeRadios[themeIndexMap.get(theme)!].checked = true;
+zoom = mediumZoom(document.querySelectorAll("[data-zoomable]"));
+applyTheme(theme);
+
+for (var i = 0; i < themeRadios.length; ++i) {
+    themeRadios[i].addEventListener(
+        "change",
+        function (this: HTMLInputElement, _: Event) {
+            localStorage.setItem("theme", this.value);
+            applyTheme(this.value);
+        },
+    );
+}
