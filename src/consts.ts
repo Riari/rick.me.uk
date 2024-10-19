cf162e01f4cf05c4e65298fc22a0ac5644affdd7
Rick Mann <3583774+Riari@users.noreply.github.com>
2024-10-19 10:34:59 +0100

    Update default OG preview

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'rick.me.uk';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'Software engineer based in the UK';
