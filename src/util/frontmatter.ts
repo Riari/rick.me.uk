db821570611f8a581a1b2c6d376a9945b35e6e16
Rick Mann <3583774+Riari@users.noreply.github.com>
2025-04-10 00:54:33 +0100

    Work on design

--- src/util/frontmatter.ts
+++ src/util/frontmatter.ts
@@ -3,11 +3,11 @@ import { ProjectStatus } from "../content/config"
 export function statusToBadgeClass(status: string) {
     switch (status) {
         case ProjectStatus.Active:
-            return 'badge badge--primary-4'
+            return 'badge badge--accent-4'
         case ProjectStatus.Complete:
-            return 'badge badge--primary-2'
+            return 'badge badge--accent-2'
         case ProjectStatus.Archived:
-            return 'badge badge--primary-3'
+            return 'badge badge--accent-3'
         default:
             return ''
     }
