f29faa6b3881e86756c1863407f17f8e476c384f
Rick Mann <3583774+Riari@users.noreply.github.com>
2025-04-13 21:08:47 +0100

    Fix some design issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
-	type: 'content',
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		heroImage: image(),
+    type: 'content',
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        heroImage: image(),
         tags: z.array(z.string()),
-	}),
+    }),
 });
 
 export enum ProjectStatus {
@@ -25,8 +25,8 @@ const projects = defineCollection({
         title: z.string(),
         description: z.string(),
         status: z.enum((projectStatuses as [string, ...string[]])),
-		weight: z.number(),
-		heroImage: image(),
+        weight: z.number(),
+        heroImage: image(),
         url: z.string().url(),
         tags: z.array(z.string()),
     }),
